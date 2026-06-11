4c5c79d7b1c1f02876fc0b5439d7c81c5d95e3de
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2026-06-11 17:23:45 +0800

    chore: add LICENSE, commitlint and basic config

---
+++ .commitlintrc.mjs
@@ -0,0 +1,12 @@
+import { readdirSync } from "node:fs";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const rootDir = dirname(fileURLToPath(import.meta.url));
+const packages = readdirSync(resolve(rootDir, "packages"));
+
+export default {
+	prompt: {
+		scopes: [...packages],
+	},
+};
